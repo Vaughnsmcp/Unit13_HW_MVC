b8060e63b93f5789081771486b04f59fb291501d
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-03-27 17:24:18 -0700

    update

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,24 @@
-const orm = require("../connection/ORM.js")
+var orm = require("../config/orm.js");
 
-const burger = {
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
+};
 
-    all:function(cb){
-        orm.all("burgers",function(res){
-            cb(res)
-        })
-    },
-    create:function(name, cb){
-
-        orm.create("burgers",[
-            "burger_name","devoured"
-        ],[
-            name,false
-        ],cb)
-     }, 
-
-     update:function(id, cb){
-         const value = "id =" + id;
-         orm.update("burgers",{
-             devoured:true
-         },value,cb)
-     }
-}
-module.exports=burger
+module.exports = burger;
